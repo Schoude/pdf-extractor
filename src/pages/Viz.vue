2b43bc7ef9fbe36d69736a5255265978b9a789d8
Marc <marcbaque1311@gmail.com>
2021-03-11 00:23:58 +0100

    [ ADD ] show mean value lihe

--- src/pages/Viz.vue
+++ src/pages/Viz.vue
@@ -9,6 +9,7 @@ import { select } from 'd3-selection';
 import { max } from 'd3-array';
 import { scaleLinear, scaleBand } from 'd3-scale';
 import { axisBottom, axisLeft } from 'd3-axis';
+import { line } from 'd3-shape';
 
 import { computed, defineComponent, onMounted } from 'vue';
 import useFileHandler from '../composables/file-handler';
@@ -25,7 +26,7 @@ export default defineComponent({
     );
     const meanFileSize = computed(() => {
       const sum = files.value.reduce((acc, file) => {
-        return acc + file.size / 1000;
+        return acc + file.size;
       }, 0);
       return Math.round(sum / files.value.length);
     });
@@ -44,6 +45,11 @@ export default defineComponent({
         .range([margin.left, width - margin.right])
         .padding(0.15);
 
+      const xScaleMean = scaleBand()
+        .domain(filesData.value.map((d) => d.name))
+        .range([margin.left, width + margin.right + margin.left])
+        .padding(0.15);
+
       const yScale = scaleLinear()
         // @ts-ignore
         .domain([0, max(filesData.value, (d) => d.value)])
@@ -59,7 +65,7 @@ export default defineComponent({
         .selectAll('rect')
         .data(filesData.value)
         .join('rect')
-        .style('fill', '#73ba9b')
+        .attr('class', 'bar')
         .attr('x', (d) => xScale(d.name))
         .attr('y', (d) => yScale(d.value))
         .attr('height', (d) => yScale(0) - yScale(d.value))
@@ -82,6 +88,28 @@ export default defineComponent({
       svg.append('g').attr('class', 'axis axis__x').call(xAxis);
 
       svg.append('g').attr('class', 'axis axis__y').call(yAxis);
+
+      const lineAvg = line()
+        // @ts-ignore
+        .x((d, i) => {
+          // @ts-ignore
+          console.log({ x: xScaleMean(d.name) });
+          // @ts-ignore
+          return xScaleMean(d.name);
+        })
+        .y(() => {
+          console.log({ y: yScale(meanFileSize.value) });
+
+          return yScale(meanFileSize.value);
+        });
+      7;
+
+      svg
+        .append('path')
+        .datum(filesData.value)
+        .attr('class', 'mean')
+        // @ts-ignore
+        .attr('d', lineAvg);
     }
 
     onMounted(() => renderBarChart());
@@ -92,6 +120,8 @@ export default defineComponent({
 </script>
 
 <style lang="scss">
+@use '../style/style' as *;
+
 .viz {
   padding: 2em;
 }
@@ -100,6 +130,16 @@ export default defineComponent({
   h1 {
     margin-left: 30px;
   }
+}
+
+.bar {
+  fill: #73ba9b;
+}
+
+.mean {
+  stroke-width: 3px;
+  stroke: $color-danger;
+  stroke-opacity: 0.5;
 }
 
 .axis {
